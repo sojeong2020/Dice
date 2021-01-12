54158849110be6e4199ab2cc1fd31ae5013d2e34
Sojeong Park <macpark09@gmail.com>
2021-01-12 17:49:41 +0000

    modify app.js and styles.css

--- app.js
+++ app.js
@@ -7,8 +7,21 @@ let randomImageSource = "images/" + randomDiceImage;
 
 let image1 = document.querySelectorAll("img")[0];
 
-image1.setAtrribute("src", randomImageSource);
+image1.setAttribute("src", randomImageSource);
 
 
+let randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
+let randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+if (randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "🤩player 1 wins"
+}
+else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "player 2 wins🤩"
+}
+else {
+    document.querySekector("h1").innerHTML = "draw"
+}
