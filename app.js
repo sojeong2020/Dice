351d7774809d26a2e284be234437c362ba36aca9
Sojeong Park <macpark09@gmail.com>
2021-01-12 17:06:13 +0000

    modify app.js index.html and styles.css

---
+++ app.js
@@ -0,0 +1,14 @@
+
+let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+let randomDiceImage = "dice" + randomNumber1 + ".png";
+
+let randomImageSource = "images/" + randomDiceImage;
+
+let image1 = document.querySelectorAll("img")[0];
+
+image1.setAtrribute("src", randomImageSource);
+
+
+
+
